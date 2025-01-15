5c477c078007b6625790db9fd989d1d97208cb77
Hahaa13 <ertdgh123@gmail.com>
2025-01-15 12:02:14 +0700

    upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html", "faq.html"],
+  content: ["index.html", "payment/success.html", "payment/cancel.html"],
   theme: {
     extend: {},
   },
